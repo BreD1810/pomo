509ad35d96d6e3df1d35f3a28aac134614ff47e4
Bradley Garrod <bradleybredgarrod@gmail.com>
2021-07-30 11:22:30 +0100

    Add ctrl-c handler to show cursor

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,30 @@ use std::time::Duration;
 use termion::{clear, cursor};
 
 fn main() {
+    ctrlc::set_handler(|| {
+        println!("{}", cursor::Show);
+        std::process::exit(1);
+    })
+    .expect("Error setting Ctrl-C handler");
+
     let mut time = 1 * 60;
     let mut stdout = io::stdout();
     println!("{}", clear::All);
     println!("{}Welcome to the Pomodoro timer", cursor::Restore);
 
     while time != 0 {
-        write!(stdout, "{}{}{}{}", clear::CurrentLine, cursor::Goto(1,2), time, cursor::Hide).unwrap();
+        write!(
+            stdout,
+            "{}{}{}{}",
+            clear::CurrentLine,
+            cursor::Goto(1, 2),
+            time,
+            cursor::Hide
+        )
+        .unwrap();
         stdout.flush().unwrap();
         time -= 1;
         sleep(Duration::from_secs(1));
     }
+    println!("Done{}", cursor::Show);
 }
