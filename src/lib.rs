162acc81405bcdf8b5c9905aeb00ad4903d5b9c6
Bradley Garrod <bradleybredgarrod@gmail.com>
2021-07-30 12:20:55 +0100

    Add CLI options for break and pomdoro length

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use clap::{value_t, App, AppSettings, Arg};
 use std::io;
 use std::io::Write;
 use std::thread::sleep;
@@ -6,30 +7,74 @@ use termion::{clear, cursor};
 
 pub struct Pomodoro {
     stdout: io::Stdout,
-    time: isize,
+
+    /// The number of minutes for each Pomodoro.
+    pomodoro_length: isize,
+
+    /// The number of minutes for each short break.
+    short: isize,
+
+    /// The number of minutes for each long break.
+    long: isize,
 }
 
 impl Pomodoro {
     pub fn new() -> Self {
-        let stdout = io::stdout();
-        let time = 60;
+        let matches = App::new("pomo")
+            .setting(AppSettings::ColoredHelp)
+            .setting(AppSettings::DisableVersion)
+            .arg(
+                Arg::with_name("pomodoro")
+                    .short("p")
+                    .long("pomdoro")
+                    .help("Number of minutes for each Pomodoro.")
+                    .default_value("25"),
+            )
+            .arg(
+                Arg::with_name("short_break")
+                    .short("s")
+                    .long("short")
+                    .help("Number of minutes for a short break.")
+                    .default_value("5"),
+            )
+            .arg(
+                Arg::with_name("long_break")
+                    .short("l")
+                    .long("long")
+                    .help("Number of minutes for a long break.")
+                    .default_value("20"),
+            )
+            .get_matches();
 
-        Pomodoro { stdout, time }
+        let pomodoro_length = value_t!(matches, "pomodoro", isize).unwrap();
+        let long = value_t!(matches, "long_break", isize).unwrap();
+        let short = value_t!(matches, "short_break", isize).unwrap();
+
+        let stdout = io::stdout();
+
+        Pomodoro {
+            stdout,
+            pomodoro_length,
+            short,
+            long,
+        }
     }
 
     pub fn run(mut self) {
-        while self.time != 0 {
+        let mut time = 60 * self.pomodoro_length;
+
+        while time != 0 {
             write!(
                 self.stdout,
                 "{}{}{}{}",
                 clear::CurrentLine,
                 cursor::Goto(1, 2),
-                self.time,
+                time,
                 cursor::Hide
             )
             .unwrap();
             self.stdout.flush().unwrap();
-            self.time -= 1;
+            time -= 1;
             sleep(Duration::from_secs(1));
         }
     }
